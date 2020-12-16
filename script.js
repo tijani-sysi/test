24aa8b353d0ee295955f9053e1fbdcebcf559435
tijani-sysi <76099143+tijani-sysi@users.noreply.github.com>
2020-12-16 10:52:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var bouton= document.getElementById('btnjs');
+var e2 = document.getElementById('sstitre');
+var e3 =document.getElementById('super titre');
+
+bouton.onclick = function() { 
+
+    e2.innerHTML = 'mon sous titre ';
+    e3.innerHTML = 'super titre';
+};
+
